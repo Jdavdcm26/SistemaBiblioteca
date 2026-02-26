2bb5be54479451c4812bd5379de41996577c8078
Jose Castro Matos <josedavidcastro@unicesar.edu.co>
2026-02-26 14:30:48 -0500

    feat: Creacion de clase 'Libro' y sus atributos

---
+++ src/main/java/com/mycompany/sistemabiblioteca/Libro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemabiblioteca;
+
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anioPublicacion;
+    private boolean disponible;
+    
+}
